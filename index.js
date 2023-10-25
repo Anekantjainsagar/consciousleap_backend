054975ed910ff0ad57241ad94d3191cc833ec31f
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2023-10-25 16:09:04 +0530

    Added https config

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ const login = require("./Routes/User/login");
 const therapist = require("./Routes/Therapist/therapist");
 const user = require("./Routes/User-things/user");
 
+const https = require("https");
+const fs = require("fs");
+
 connect();
 
 app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 5000;
+// Load SSL certificate and private key
+const options = {
+  key: fs.readFileSync("/home/ubuntu/ssl/privkey.pem"),
+  cert: fs.readFileSync("/home/ubuntu/ssl/fullchain.pem"),
+};
+
+const httpsServer = https.createServer(options, app);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -22,6 +34,6 @@ app.use("/api/login", login);
 app.use("/api/user", user);
 app.use("/api/therapist", therapist);
 
-app.listen(process.env.PORT, () => {
+httpsServer.listen(port, () => {
   console.log(`App is listening on port ${process.env.PORT}`);
 });
