423b63fc2149432894303fdfde9e2c4dc77083ff
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-05-17 00:31:17 +0530

    Backend commit

--- Routes/admin.js
+++ Routes/admin.js
@@ -84,6 +84,51 @@ admin.post("/delete-blog/:id", async (req, res) => {
   res.status(200).send(response);
 });
 
+// Like or Unlike a blog
+admin.post("/toggle-like/:id", async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.body;
+
+  try {
+    const blog = await Blog.findById(id);
+    if (!blog) {
+      return res.status(404).send("Blog not found");
+    }
+
+    const index = blog.likes.indexOf(userId);
+    if (index === -1) {
+      blog.likes.push(userId);
+    } else {
+      blog.likes.splice(index, 1);
+    }
+
+    await blog.save();
+    res.status(200).send(blog);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Add a comment to a blog
+admin.post("/add-comment/:id", async (req, res) => {
+  const { id } = req.params;
+  const { text, postedBy } = req.body;
+
+  try {
+    const blog = await Blog.findById(id);
+    if (!blog) {
+      return res.status(404).send("Blog not found");
+    }
+    blog.comments.push({ text, postedBy });
+    await blog.save();
+    res.status(200).send(blog);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 // Orders post
 admin.get("/get-orders", async (req, res) => {
   try {
