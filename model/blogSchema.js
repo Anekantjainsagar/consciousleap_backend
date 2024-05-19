7bea8a588c9cca88b26a42584ac7889b3300d0c9
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-05-19 17:08:56 +0530

    Comment postby column updated

--- model/blogSchema.js
+++ model/blogSchema.js
@@ -1,33 +1,30 @@
-  const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-  const blogSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    image: String,
-    date: {
-      type: Date,
-      default: Date.now(),
+const blogSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  image: String,
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
     },
-    likes: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Users", 
+  ],
+  comments: [
+    {
+      text: String,
+      postedBy: String,
+      date: {
+        type: Date,
+        default: Date.now(),
       },
-    ],
-    comments: [
-      {
-        text: String,
-        postedBy: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Users",
-        },
-        date: {
-          type: Date,
-          default: Date.now(),
-        },
-      },
-    ],
-  });
+    },
+  ],
+});
 
-  const Blog = mongoose.model("Blogs", blogSchema);
-  module.exports = Blog;
+const Blog = mongoose.model("Blogs", blogSchema);
+module.exports = Blog;
