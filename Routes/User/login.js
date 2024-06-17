5ef29d95c04d428d980b6cf70cc475230c0f429a
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-06-17 13:07:47 +0530

    Email changed

--- Routes/User/login.js
+++ Routes/User/login.js
@@ -77,13 +77,8 @@ login.post("/send-test-mail", async (req, res) => {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-      rel="stylesheet"
-    />
     <style>
+      @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
       * {
         font-family: "Montserrat", sans-serif;
       }
