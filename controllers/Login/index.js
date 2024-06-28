f84270f11732e9ce4b13ce6f775414741f699339
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-06-28 13:51:16 +0530

    asdf

--- controllers/Login/index.js
+++ controllers/Login/index.js
@@ -447,7 +447,17 @@ exports.updateQuestionnaire = async (req, res) => {
 </html>
 `;
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--single-process",
+      "--no-zygote",
+      "--disable-gpu",
+    ],
+  });
   const page = await browser.newPage();
   await page.setContent(html);
   const pdfBuffer = await page.pdf({ format: "A4" });
@@ -472,7 +482,7 @@ exports.updateQuestionnaire = async (req, res) => {
       },
     ],
   });
-
+  console.log(result);
   res.send(questionnaire);
 
   // await pdf
